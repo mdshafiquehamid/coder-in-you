7abe51030000653fd7bcc5f4aa623592dfc83fa3
mdshafiquehamid <39638811+mdshafiquehamid@users.noreply.github.com>
2018-06-08 18:38:16 +0800

    Updated 080618
    Coder in you class 080618

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+var fred = ['fred', 'flintstone', 'fred@gmail.com', 50];
+
+console.log('fred=', fred);
+console.info('fred=', fred);
+console.warn('fred=', fred);
+console.error('fred=', fred);
+
+
+function printData() {
+
+    for(var person = 0; person < fred.length; );
+
+}
